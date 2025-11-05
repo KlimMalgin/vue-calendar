80b0d87d8e19356b990ae02dba17844d9275bea7
Brunetkin Andrey <fxklim@gmail.com>
2025-11-05 14:17:21 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Calendar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calendar",
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
