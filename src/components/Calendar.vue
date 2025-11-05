d84a8de1b354e9c9c831b4277ae8b8c5a56fd99a
Brunetkin Andrey <fxklim@gmail.com>
2025-11-05 15:46:17 +0500

    day numbers center

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -240,8 +240,8 @@ export default {
 }
 .day-cell {
   display: flex;
-  align-items: flex-start;
-  justify-content: flex-end;
+  align-items: center;
+  justify-content: center;
   padding: 6px;
   border-radius: 6px;
   cursor: pointer;
